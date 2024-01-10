69bd800b1644d211314064f5f15d5dd29008c0ba
Renzel John Tubat <renzeljohntubat27@gmail.com>
2024-01-10 11:44:23 +0800

    update launcher

--- address-book.bat
+++ address-book.bat
@@ -2,6 +2,6 @@ cd %USERPROFILE%/Desktop/address-book-main/address-book
 
 npm install
 
-start cd %USERPROFILE%/Desktop/address-book-main/go-api/launch-api.bat
+start %USERPROFILE%/Desktop/address-book-main/go-api/launch-api.bat
 
 npm start
